474e7a475915532e9cd758f18e2f6027649f64c8
Phil Thomas <pthomas551@gmail.com>
2015-11-30 23:43:56 -0600

    Add 7.2

---
+++ week-7/eloquent.js
@@ -0,0 +1,41 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+/*
+var foo = 444;
+foo * 23 / 12;
+
+var food = String(prompt("What is your favorite food?"));
+alert("Your favorite food is " + food);
+*/
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+/*
+for (var number = 1; number <= 100; number++) {
+  if (number % 3 == 0 && number % 5 == 0) console.log('FizzBuzz');
+  else if (number % 3 == 0) console.log('Fizz');
+  else if (number % 5 == 0) console.log('Buzz');
+  else console.log(number);
+}
+*/
+// Functions
+
+// Complete the `minimum` exercise.
+/*
+function minimum(a, b) {
+  if (a < b) console.log(a);
+  else console.log(b);
+}
+*/
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+var me {
+  age: 32;
+  favoriteFoods: ['burritos','pasta','cheese'];
+  quirk: 'techno fan';
+}
